6abf17ac5b27e8c4ddf808d2aa559c33c947e44e
Oscar Manuel Hortua <oscarmanuel4502@outlook.com>
2023-09-02 20:01:07 -0500

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@mui/material/Button'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <h1> Lista de tareas </h1>
+      <label>
+        Nueva Tarea
+        <input type='text'></input>
+      </label>
+      <Button  variant="text" color="primary">
+        Guardar
+      </Button>
+      <Button variant="text" color="secondary">
+        Consutar
+      </Button>
+      <Button variant="text" color="inherit">
+        Estado
+      </Button>
+     </div>
     </div>
   );
 }
